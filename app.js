532a3a05673e450702ed309b6c3e3ba998d16f6f
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-03-05 11:58:58 +0100

    angular initialised in html and javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+(function (){
+    'use strict'; // used to make sure careless mistakes are not made
+
+    //angular methodmodule to start app
+    angular.module('myFirstApp', [])
+    //controller represents the view model, a way a combining HTML and Javascript
+    .controller('myFirstController', function(){
+
+    });
+})();
